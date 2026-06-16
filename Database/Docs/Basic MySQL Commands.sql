aa047711afd5603996cfd27a64498d391182d5d6
David Gordon <dgordon412@gmail.com>
2026-06-15 20:49:06 -0800

    Update Basic MySQL Commands.sql

--- Database/Docs/Basic MySQL Commands.sql
+++ Database/Docs/Basic MySQL Commands.sql
@@ -1,3 +1,5 @@
+-- Common MySQL Commands
+
 SELECT * FROM futuremtn_central_coast.cubedata;
 
 USE futuremtn_central_coast;
@@ -28,21 +30,4 @@ ALTER TABLE firedata AUTO_INCREMENT = 1;
 ALTER TABLE patchdata AUTO_INCREMENT = 1;
 
 
-USE futuremtn_central_coast;
-CREATE TABLE `firedata` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `importRunId` int NOT NULL,
-  `scenarioRunId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `warmingIdx` int NOT NULL,
-  `sourceFile` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `year` int NOT NULL,
-  `month` int NOT NULL,
-  `level` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `basinID` int NOT NULL,
-  `hillID` int DEFAULT NULL,
-  `zoneID` int DEFAULT NULL,
-  `patchID` bigint DEFAULT NULL,
-  `burn` float NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IX_FireData_scenarioRunId_warmingIdx_year_month_zoneID_patchID` (`scenarioRunId`,`warmingIdx`,`year`,`month`,`zoneID`,`patchID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1006877441 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
