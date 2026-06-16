46c75b9502e5c76ed8086a5bbdc4345d3cdd2577
David Gordon <dgordon412@gmail.com>
2026-06-15 20:36:04 -0800

    Update Basic MySQL Commands.sql

--- Database/Docs/Basic MySQL Commands.sql
+++ Database/Docs/Basic MySQL Commands.sql
@@ -10,10 +10,39 @@ UNION ALL SELECT 'patchdata', COUNT(*) FROM patchdata
 UNION ALL SELECT 'stratumdata', COUNT(*) FROM stratumdata
 UNION ALL SELECT 'terraindata', COUNT(*) FROM terraindata;
 
+
+SHOW CREATE TABLE cubedata;
+SHOW CREATE TABLE firedata;
+SHOW CREATE TABLE patchdata;
+
+
 USE futuremtn_central_coast;
-
 SET FOREIGN_KEY_CHECKS = 0;
-
 TRUNCATE TABLE cubedata;
-
+TRUNCATE TABLE patchdata;
+TRUNCATE TABLE firedata;
 SET FOREIGN_KEY_CHECKS = 1;
+
+ALTER TABLE cubedata AUTO_INCREMENT = 1;
+ALTER TABLE firedata AUTO_INCREMENT = 1;
+ALTER TABLE patchdata AUTO_INCREMENT = 1;
+
+
+USE futuremtn_central_coast;
+CREATE TABLE `firedata` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `importRunId` int NOT NULL,
+  `scenarioRunId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `warmingIdx` int NOT NULL,
+  `sourceFile` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `year` int NOT NULL,
+  `month` int NOT NULL,
+  `level` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `basinID` int NOT NULL,
+  `hillID` int DEFAULT NULL,
+  `zoneID` int DEFAULT NULL,
+  `patchID` bigint DEFAULT NULL,
+  `burn` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IX_FireData_scenarioRunId_warmingIdx_year_month_zoneID_patchID` (`scenarioRunId`,`warmingIdx`,`year`,`month`,`zoneID`,`patchID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1006877441 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
